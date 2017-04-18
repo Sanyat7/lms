8e76a2d04ff1c55da4ea0eb816399b3330b466c3
madridistaSanyat <sanyatcr7@hotmail.com>
2017-04-18 10:04:05 +0545

    Added Copy Model

---
+++ LibraryManagementSystem/Models/Copy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Copy
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public int BookId
+        {
+            get;
+            set;
+        }
+        public virtual Book Book
+        {
+            get;
+            set;
+        }
+        public int ShelfId
+        {
+            get;
+            set;
+        }
+        public virtual Shelf Shelf
+        {
+            get;
+            set;
+        }
+    }
+}
