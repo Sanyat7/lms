0aac6fbdcad8ef3c04197d8201553f99ff1842a3
madridistaSanyat <sanyatcr7@hotmail.com>
2017-04-25 13:51:05 +0545

    Add Author ViewModel

--- LibraryManagementSystem/Controllers/BooksController.cs
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using LibraryManagementSystem.Models;
+using LibraryManagementSystem.ViewModels;
 
 namespace LibraryManagementSystem.Controllers
 {
@@ -127,6 +128,117 @@ namespace LibraryManagementSystem.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        //For the Authors
+        public ActionResult Authors(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public ActionResult EditAuthors(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            PopulateAssignedAuthorData(book);
+            return View(book);
+        }
+        private void PopulateAssignedAuthorData(Book book)
+        {
+            var allAuthors = db.Authors;
+            var bookAuthors = new HashSet<int>(book.Authors.Select(s => s.Id));
+            var viewModel = new List<AssignedAuthorData>();
+            foreach (var author in allAuthors)
+            {
+                viewModel.Add(new AssignedAuthorData
+                {
+                    AuthorId = author.Id,
+                    Name = author.FirstName + " " + author.MiddleName + " " + author.LastName,
+                    Assigned = bookAuthors.Contains(author.Id)
+                });
+            }
+            ViewBag.Authors = viewModel;
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditAuthors(int? id, string[] selectedAuthors)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bookToUpdate = db.Books
+               .Include(i => i.Authors)
+               .Where(i => i.Id == id)
+               .Single();
+
+            try
+            {
+                UpdateBookAuthor(selectedAuthors, bookToUpdate);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Authors", new { id = id });
+            }
+            catch (Exception /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again!!");
+            }
+
+            PopulateAssignedAuthorData(bookToUpdate);
+            return View(bookToUpdate);
+        }
+
+        private void UpdateBookAuthor(string[] selectedAuthors, Book bookToUpdate)
+        {
+            if (selectedAuthors == null)
+            {
+                bookToUpdate.Authors = new List<Author>();
+                return;
+            }
+
+            var selectedAuthorsHS = new HashSet<string>(selectedAuthors);
+            var bookAuthors = new HashSet<int>
+                (bookToUpdate.Authors.Select(c => c.Id));
+            foreach (var author in db.Authors)
+            {
+                if (selectedAuthorsHS.Contains(author.Id.ToString()))
+                {
+                    if (!bookAuthors.Contains(author.Id))
+                    {
+                        bookToUpdate.Authors.Add(author);
+                    }
+                }
+                else
+                {
+                    if (bookAuthors.Contains(author.Id))
+                    {
+                        bookToUpdate.Authors.Remove(author);
+                    }
+                }
+            }
         }
     }
 }
---
+++ LibraryManagementSystem/ViewModels/AssignedAuthorData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.ViewModels
+{
+    public class AssignedAuthorData
+    {
+        public int AuthorId
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public bool Assigned
+        {
+            get;
+            set;
+        }
+    }
+}
